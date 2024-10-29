1315c5241ca8dd41958a9e68b84fcd8263ed1547
Arina Bilan <arina.bilan@usach.cl>
2024-10-29 01:26:19 -0300

    Se corrige un poco el estilo y se agregan controles, faltan los setters

--- src/components/Evaluate.jsx
+++ src/components/Evaluate.jsx
@@ -2,14 +2,23 @@ import React, { useState, useEffect } from "react";
 import loanService from "../services/loan.service";
 import documentService from "../services/document.service";
 import clientService from "../services/client.service";
-import { Typography, Container, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Button} from '@mui/material';
+import { Typography, TextField, MenuItem,Container, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Button, Link,Switch } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import { useNavigate } from 'react-router-dom';
 
 const Evaluate = () => {
 
     const [clientDocuments, setClientDocument] = useState([]);
     const [clientSavingCapacity, setClientSavingCapacity] = useState({});
-    const [clientDates, setClientDates] = useState({});
+    const [clientDates, setClientDates] = useState({
+        monthSalary: 0,
+        date: 0,
+        initialContract: 0,
+        dicom: false,
+        type: 2,
+        mediaSalary: 0,
+        monthlyDebt: 0
+    });
     const [clientSolicitude, setClientSolicitude] = useState({});
     const [client, setClient] = useState({});
 
@@ -45,29 +54,164 @@ const Evaluate = () => {
                 Evaluación de Solicitud
             </Typography>
             
-            <Typography>Nombre: {client.name} {client.surname}</Typography>
+            {/* <Typography>Nombre: {client.name} {client.surname}</Typography>
             <Typography>Fecha de nacimiento: {client.birthday}</Typography>
+            <br/> */}
+
+            <TableContainer component={Paper} style={{ marginTop: '40px' }}>
+                <Table style={{ minWidth: '1000px' }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell style={{fontWeight:'bold'}}>Cliente</TableCell>
+                            <TableCell style={{fontWeight:'bold'}}>Tipo de Préstamo</TableCell>
+                            <TableCell style={{fontWeight:'bold'}}>Monto</TableCell>
+                            <TableCell style={{fontWeight:'bold'}}>Tasa de Interés</TableCell>
+                            <TableCell style={{fontWeight:'bold'}}>Fecha</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow key={solicitudCliente.id}>
+                        <TableCell>{client.name} {client.surname}</TableCell>
+                        <TableCell>{solicitudCliente.loanType.type}</TableCell>
+                            <TableCell>{solicitudCliente.amount.toLocaleString()} CLP</TableCell>
+                            <TableCell>{(solicitudCliente.interestRate * 100).toFixed(2)}%</TableCell>
+                            <TableCell>{new Date(solicitudCliente.date).toLocaleDateString()}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
             <br/>
-            <Typography>Ingreso Mensual: {clientDates.monthSalary} CLP</Typography>
-            <Typography>Años con banco: {Math.floor(clientDates.date / 12)} años </Typography>
-            <Typography>Años trabajando: {Math.floor(clientDates.initialContract / 12)} años </Typography>
-            <Typography>Esta en Dicom: {clientDates.dicom ? "Sí" : "No"} </Typography>
-            <Typography>Tipo de trabajador: {clientDates.type === 1 ? "Independiente" : "Con contrato"} </Typography>
-            <Typography>Promedio de ingresos en ultimos 2 años: {clientDates.mediaSalary} CLP</Typography>
-            <Typography>Deuda mensual: {clientDates.monthlyDebt} CLP</Typography>
+
+            <Typography variant="h5" gutterBottom>
+                Datos del Cliente
+            </Typography>
+
+            <Grid container rowSpacing={0} columnSpacing={2}>
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Ingreso Mensual:</Grid>
+                <Grid size={4} style={{textAlign: 'left'}}>
+                    <TextField
+                        type="number"
+                        value={clientDates.monthSalary}
+                        required
+                        size="small"
+                    />                    
+                 CLP</Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Años con banco:</Grid>
+                <Grid size={4} style={{textAlign: 'left'}}>
+                    <TextField
+                        type="number"
+                        value={clientDates.date}
+                        required
+                        size="small"
+                    />                         
+                     meses </Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Años trabajando:</Grid>
+                <Grid size={4} style={{textAlign: 'left'}}>
+                    <TextField
+                        type="number"
+                        value={clientDates.initialContract}
+                        required
+                        size="small"
+                    />                         
+                     meses </Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Esta en Dicom:</Grid>
+                <Grid size={4} style={{textAlign: 'left'}}>
+                    No <Switch checked={clientDates.dicom} /> Si
+                </Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Tipo de trabajador:</Grid>
+                <Grid size={4} style={{textAlign: 'left'}}>
+                    <TextField
+                        select
+                        value={clientDates.type}
+                        size="small"
+                    >
+                        <MenuItem key={1} value={1}>
+                            Independiente
+                        </MenuItem>
+                        <MenuItem key={2} value={2}>
+                            Con contrato
+                        </MenuItem>
+                    </TextField>                    
+                    </Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Promedio de ingresos en ultimos 2 años:</Grid>
+                <Grid size={4} style={{textAlign: 'left'}}>
+                    <TextField
+                        type="number"
+                        value={clientDates.mediaSalary}
+                        required
+                        size="small"
+                    />                         
+                     CLP</Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Deuda mensual:</Grid>
+                <Grid size={4} style={{textAlign: 'left'}}>
+                <TextField
+                        type="number"
+                        value={clientDates.monthlyDebt}
+                        required
+                        size="small"
+                    />                         
+                     CLP</Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+            </Grid>
             <br/>
-            <Typography>Saldo enn cuenta ahorro: {clientSavingCapacity.balance} CLP</Typography>
-            <Typography>Suma de retiros en ultimos 12 meses: {clientSavingCapacity.withdrawals} CLP </Typography>
-            <Typography>Historial de ahorro consistente: {clientSavingCapacity.withdrawal ? "Sí" : "No"} </Typography>
-            <Typography>Suma de depositos mensuales de ultimos 12 meses: {clientSavingCapacity.deposits} CLP </Typography>
-            <Typography>Años de antiguidad de la cuenta de ahorro: {clientSavingCapacity.yearsSavings} años</Typography>
-            <Typography>Suma de retiros en ultimos 6 meses: {clientSavingCapacity.recentWithdrawals} CLP</Typography>
+
+            <Typography variant="h5" gutterBottom>
+                Capacidad de Ahorro
+            </Typography>
+
+            <Grid container rowSpacing={1} columnSpacing={2}>
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Saldo enn cuenta ahorro:</Grid>
+                <Grid size={4}>
+                    {clientSavingCapacity.balance} CLP
+                    </Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Suma de retiros en ultimos 12 meses:</Grid>
+                <Grid size={4}>{clientSavingCapacity.withdrawals} CLP </Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Historial de ahorro consistente:</Grid>
+                <Grid size={4}>{clientSavingCapacity.withdrawal ? "Sí" : "No"} </Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Suma de depositos mensuales de ultimos 12 meses:</Grid>
+                <Grid size={4}>{clientSavingCapacity.deposits} CLP </Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Años de antiguidad de la cuenta de ahorro:</Grid>
+                <Grid size={4}>{clientSavingCapacity.yearsSavings} años</Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+
+                <Grid size={4} style={{textAlign: 'left', fontWeight:'bold'}}>Suma de retiros en ultimos 6 meses:</Grid>
+                <Grid size={4}>{clientSavingCapacity.recentWithdrawals} CLP</Grid>
+                <Grid size={4}><Link>Ver documento</Link></Grid>
+            </Grid>
+            <br/>
+
             <Button
                 variant="contained"
                 color="secondary"
-                
             >
                 Evaluar
+            </Button>
+            <Button
+                variant="contained"
+                color="secondary"
+                onClick={() => navigate('/allsolicitudes')}
+            >
+                Volver
             </Button>
         </Container>
     );
